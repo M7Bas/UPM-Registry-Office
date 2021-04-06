cd978816f74693358650dade7aa5364e9f81245a
M7Bas <74269001+M7Bas@users.noreply.github.com>
2021-04-06 22:09:51 +0300

    Update Undergraduate.java

--- src/sa/edu/upm/Undergraduate.java
+++ src/sa/edu/upm/Undergraduate.java
@@ -3,9 +3,16 @@ package sa.edu.upm;
 import java.util.List;
 
 public class Undergraduate extends Student{
+    private String major;
     private List<Course> courses;
 
-    public Undergraduate(String name, String ID) {
+    public Undergraduate(String ID,String name, String major) {
         super(name, ID);
+        this.major = major;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +" "+ major;
     }
 }
